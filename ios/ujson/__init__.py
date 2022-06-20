4ffdea2ea6e3b8c661ff0f49421bd1bca3f479b0
yumoqing <yumoqing@gmail.com>
2022-06-20 14:33:56 +0800

    bugfix

--- ios/ujson/__init__.py
+++ ios/ujson/__init__.py
@@ -1,13 +1,24 @@
 import os
-from os.path import join
+import os
+import sh
+from kivy_ios.toolchain import PythonRecipe, shprint
 
-from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
+class UJsonRecipe(PythonRecipe):
+	version = "5.3.0"
+	url = 'https://files.pythonhosted.org/packages/92/38/a8c8d8cdacd586e0e66673ca60daf295a79cd5b4fae72a25f1bfa482554d/ujson-5.3.0.tar.gz'
+	depends = ["python", "setuptools" ]
 
-class UJsonRecipe(CompiledComponentsPythonRecipe):
-	version = '1.35'
-	url = 'https://pypi.python.org/packages/source/u/ujson/ujson-{version}.tar.gz'
-	depends = ['hostpython3']
-	call_hostpython_via_targetpython = False
+	def install(self):
+		arch = list(self.filtered_archs)[0]
+		build_dir = self.get_build_dir(arch.arch)
+		os.chdir(build_dir)
+		hostpython = sh.Command(self.ctx.hostpython)
+		build_env = arch.get_env()
+		dest_dir = os.path.join(self.ctx.dist_dir, "root", "python")
+		build_env['PYTHONPATH'] = os.path.join(dest_dir, 'lib', 'python3.7', 'site-packages')
+		shprint(hostpython, "setup.py", "build_ext", "--prefix", dest_dir, _env=build_env)
+		shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+
 
 recipe = UJsonRecipe()
