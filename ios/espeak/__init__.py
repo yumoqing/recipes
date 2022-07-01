023f6f8fb4226d49bb3a138bfcec546de4a51ade
yumoqing <yumoqing@gmail.com>
2022-07-01 11:23:16 +0800

    bugfix

---
+++ ios/espeak/__init__.py
@@ -0,0 +1,31 @@
+from kivy_ios.toolchain import Recipe, shprint
+from kivy_ios.context_managers import cd
+from os.path import join, dirname, abspath
+import sh
+import os
+import fnmatch
+
+class EspeakRecipe(Recipe):
+	'''
+	'''
+	version = '1.48.04'
+	url = 'http://sourceforge.net/projects/espeak/files/espeak/espeak-1.48/espeak-1.48.04-source.zip'
+	patches = []
+	dependents = ['portaudio']
+	library = "libportaudio.a"
+
+	def build_arch(self, arch):
+		build_env = arch.get_env()
+		configure = sh.Command(join(self.build_dir, "configure"))
+		shprint(configure,
+				"CC={}".format(build_env["CC"]),
+				"LD={}".format(build_env["LD"]),
+				"CFLAGS={}".format(build_env["CFLAGS"]),
+				"LDFLAGS={}".format(build_env["LDFLAGS"]),
+				"--prefix=/",
+				"--host={}".format(arch.triple),
+				"--disable-shared")
+		shprint(sh.make, 'clean')
+		shprint(sh.make, self.ctx.concurrent_make)
+
+recipe = EspeakRecipe()
---
+++ ios/m3u8/__init__.py
@@ -0,0 +1,8 @@
+
+from kivy_ios.toolchain import PythonRecipe
+from kivy_ios.recipes.pypi_info import get_version_url
+class TmpRecipe(PythonRecipe):
+   version, url = get_version_url('m3u8', version='')
+   depends = ["python"]
+
+recipe = TmpRecipe()
