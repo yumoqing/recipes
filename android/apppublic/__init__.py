95d12b78c21ce91ec58e3b0fab4668d52c1c0fac
yumoqing <yumoqing@gmail.com>
2022-08-18 11:34:47 +0800

    bugfix

--- android/apppublic/__init__.py
+++ android/apppublic/__init__.py
@@ -2,15 +2,16 @@ import os
 import sh
 from pythonforandroid.logger import shprint, info
 from pythonforandroid.util import current_directory
+from pythonforandroid.recipes.pypi_info import get_version_url
 from os.path import join
 
 from pythonforandroid.recipe import PythonRecipe
 
 class ApppublicRecipe(PythonRecipe):
-	version = '5.1.16'
-	url = 'http://kimird.com/pymodules/appPublic-5.1.16.tar.gz'
+	# version = '5.1.16'
+	# url = 'http://kimird.com/pymodules/appPublic-5.1.16.tar.gz'
+	version, url = get_version_url('apppublic')
 	depends = ['python3']
-	site_packages_name = 'apppublic'
 	call_hostpython_via_targetpython = False
 
 recipe = ApppublicRecipe()
--- ios/apppublic/__init__.py
+++ ios/apppublic/__init__.py
@@ -1,9 +1,8 @@
-from kivy_ios.recipes.pypi_info import get_version_url
+
 from kivy_ios.toolchain import PythonRecipe
-class apppublicRecipe(PythonRecipe):
-   # version, url = get_version_url('apppublic')
-   version = "5.1.16"
-   url = 'http://kimird.com/pymodules/appPublic-5.1.16.tar.gz'
+from kivy_ios.recipes.pypi_info import get_version_url
+class TmpRecipe(PythonRecipe):
+   version, url = get_version_url('apppublic', version='')
    depends = ["python"]
 
-recipe = apppublicRecipe()
+recipe = TmpRecipe()
